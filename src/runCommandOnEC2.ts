00d4e70701e54901d65ce2c82e4e706339a7875d
Asher Hwang <dev@hsh.kr>
2021-10-08 14:43:14 +0900

    Refactor codes

--- src/runCommandOnEC2.ts
+++ src/runCommandOnEC2.ts
@@ -18,30 +18,55 @@ export async function runCommandOnEC2(ipAddress: string): Promise<void> {
       .on('ready', () => {
         console.log('Client :: ready');
         ssh.exec(
-          `
-            sudo apt-get update && 
-            sudo chmod 777 /var/cache/debconf/ &&
-            sudo chmod 777 /var/cache/debconf/passwords.dat &&
-            (echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-            sudo apt-get install -y -q) &&
-            sudo apt-get install -yq nodejs npm libcap2-bin &&
-            (
-              ${nvmInstallCode}
-            ) &&
-            export NVM_DIR="$HOME/.nvm" &&
-            [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  &&
-            [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  &&
-            nvm install 14.17.5 && nvm use 14.17.5 &&
-            node -v &&
-            git clone https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GIT_OWNER}/${process.env.GIT_REPO}.git &&
-            cd ${process.env.GIT_REPO} &&
-            git checkout ${process.env.GIT_REF} &&
-            npm i && 
-            sudo npm i -g pm2 &&
-            npm run build && 
-            sudo setcap cap_net_bind_service=+ep \`readlink -f \\\`which node\\\`\` &&
-            pm2 start server.js
-          `,
+          [
+            // apt 사용 준비
+            'sudo apt-get update',
+
+            // apt-get install 시에 에러를 방지하기 위한 설치
+            // TODO: 정확히 무슨 문제인지 조사하고 설명하는 주석 추가하기
+            'sudo chmod 777 /var/cache/debconf/',
+            'sudo chmod 777 /var/cache/debconf/passwords.dat',
+            `echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+            sudo apt-get install -y -q`,
+
+            // nodejs npm libcap2-bin 설치
+            // libcap2-bin 은 80번 포트 사용 권한 부여를 위함이다.
+            'sudo apt-get install -yq nodejs npm libcap2-bin',
+
+            // nvm 설치하기
+            nvmInstallCode,
+            'export NVM_DIR="$HOME/.nvm"',
+            '[ -s "$NVM_DIR/nvm.sh" ]',
+            '. "$NVM_DIR/nvm.sh"',
+            '[ -s "$NVM_DIR/bash_completion" ]',
+            '. "$NVM_DIR/bash_completion"',
+
+            // nodejs & npm 버전업!
+            'nvm install 14.17.5',
+            'nvm use 14.17.5',
+            'node -v',
+
+            // 깃 클론!!
+            `git clone https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GIT_OWNER}/${process.env.GIT_REPO}.git`,
+            `cd ${process.env.GIT_REPO}`,
+            `git checkout ${process.env.GIT_REF}`,
+
+            // npm 의존설 설치
+            'npm i',
+
+            // pm2 설치
+            'sudo npm i -g pm2',
+
+            // nodejs 에게 80 포트를 사용할 권한을 주기
+            'sudo setcap cap_net_bind_service=+ep `readlink -f \\`which node\\``',
+
+            // 빌드하고 시작!!!
+            'npm run build',
+            'pm2 run npm -- start',
+          ]
+            .map((command) => `(\n${command}\n)`)
+            .join(' && '),
+
           (err, stream) => {
             if (err) return reject(err);
 
